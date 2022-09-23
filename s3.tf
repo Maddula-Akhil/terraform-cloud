812f42da4ca757542d1518e46e78bd624a36b627
Maddula-Akhil <107178987+Maddula-Akhil@users.noreply.github.com>
2022-09-23 17:36:43 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -5,28 +5,6 @@ resource "aws_s3_bucket" "sss-bucket" {
     Environment = "Dev"
   }
 }
-resource "aws_s3_bucket_policy" "all_access" {
-  bucket = aws_s3_bucket.sss-bucket.id
-  policy = data.aws_iam_policy_document.all_access.json
-}
-
-data "aws_iam_policy_document" "all_access" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions = [
-      "s3:*"
-    ]
-
-    resources = [
-      "${aws_s3_bucket.sss-bucket.arn}",
-      "${aws_s3_bucket.sss-bucket.arn}/*"
-    ]
-  }
-}
 
 data "aws_s3_bucket" "all" {
  bucket = "terraform-scriptbucket"
